39d25be3686515347380d558947a7d547bb05fbc
lowcq <nasyua84865@gmail.com>
2024-11-15 22:59:56 +0300

    add 3.5.A

---
+++ ASD 5.1.A/ASD 5.1.A/ASD 5.1.A.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include "conio.h"
+using namespace std;
+
+	
+int fibonacchi(int n)
+{
+	if (n < 2)
+	{
+		return n;
+	}
+	else
+	{
+		return fibonacchi(n - 2) + fibonacchi(n - 1);
+	}
+}
+
+
+	int main()
+	{
+
+		setlocale(LC_ALL, "Rus");
+		
+		int n;
+		cout << "Введите число от 0 до 45" << endl;
+		cin >> n;
+
+		cout << fibonacchi(n);
+
+		return 0;
+	}
